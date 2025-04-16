08d5707ac2f0312b32940de21b314b03037cbd61
sachinsinghpatwal <sachinsingh71004@gmail.com>
2025-04-16 11:56:26 +0530

    established likes and tweet endpoints .

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,33 +1,134 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import {Like} from "../models/like.model.js";
+import {ApiError} from "../utils/ApiError.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
+import {asyncHandler} from "../utils/asyncHandler.js";
+import {Video} from "../models/video.model.js";
+import mongoose from "mongoose";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
-})
+  //TODO: toggle like on video
+  const {videoId: video} = req.params;
+  const {_id: likedBy} = req.user;
+  let status;
+  const videoExist = await Video.findById(video);
+  if (!videoExist) throw new ApiError(400, "video does not exist");
+  const existedVideoLikedStatus = await Like.findOne({
+    video,
+    likedBy,
+  });
+  if (existedVideoLikedStatus) {
+    await Like.deleteOne({
+      _id: existedVideoLikedStatus._id,
+    });
+    status = {videosLikedStatus: false};
+  } else {
+    await Like.create({video, likedBy});
+    status = {videosLikedStatus: true};
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, status, "video status fetched successfully"));
+});
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
-    //TODO: toggle like on comment
-
-})
+  //TODO: toggle like on comment
+  const {commentId: comment} = req.params;
+  const {_id: likedBy} = req.user;
+  let status;
+  const commentExist = await Video.findById(comment);
+  if (!commentExist) throw new ApiError(400, "comment does not exist");
+  const existedCommentLikedStatus = await Like.findOne({
+    comment,
+    likedBy,
+  });
+  if (existedCommentLikedStatus) {
+    await Like.deleteOne({
+      _id: existedCommentLikedStatus._id,
+    });
+    status = {commentLikedStatus: false};
+  } else {
+    await Like.create({comment, likedBy});
+    status = {commentLikedStatus: true};
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, status, "comment status fetched successfully"));
+});
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
-    //TODO: toggle like on tweet
-}
-)
+  //TODO: toggle like on tweet
+  const {tweetId: tweet} = req.params;
+  const {_id: likedBy} = req.user;
+  let status;
+  const tweetExist = await Video.findById(tweet);
+  if (!tweetExist) throw new ApiError(400, "tweet does not exist");
+  const existedTweetLikedStatus = await Like.findOne({
+    tweet,
+    likedBy,
+  });
+  if (existedTweetLikedStatus) {
+    await Like.deleteOne({
+      _id: existedTweetLikedStatus._id,
+    });
+    status = {tweetLikedStatus: false};
+  } else {
+    await Like.create({tweet, likedBy});
+    status = {tweetLikedStatus: true};
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, status, "tweet status fetched successfully"));
+});
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
-})
+  //TODO: get all liked videos
 
-export {
-    toggleCommentLike,
-    toggleTweetLike,
-    toggleVideoLike,
-    getLikedVideos
-}
+  const videos = await Like.aggregate([
+    {
+      $match: {
+        likedBy: new mongoose.Types.ObjectId(req.user._id),
+        video: {$ne: null},
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "video",
+      },
+    },
+    {
+      $unwind: "$video",
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "video.owner",
+        foreignField: "_id",
+        as: "video.owner",
+      },
+    },
+    {
+      $unwind: "$video.owner",
+    },
+    {
+      $project: {
+        _id: 0,
+        videoFile: "$video.videoFile",
+        thumbnail: "$video.thumbnail",
+        title: "$video.title",
+        duration: "$video.duration",
+        views: "$video.views",
+        isPublished: "$video.isPublished",
+        createdAt: "$video.createdAt",
+        username: "$owner.username",
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videos, "videos fetched successfully"));
+});
+
+export {toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos};
--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,29 +1,85 @@
-import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, {isValidObjectId} from "mongoose";
+import {Tweet} from "../models/tweet.model.js";
+import {ApiError} from "../utils/ApiError.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
+import {asyncHandler} from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-})
+  const {content} = req.body;
+  const {_id: owner} = req.user;
+  if (!owner) {
+    throw new ApiError(404, "unauthorized to access resource");
+  }
+  if (!content) {
+    throw new ApiError(
+      404,
+      "Empty content , please provide some value in content"
+    );
+  }
+  const tweet = await Tweet.create({
+    content,
+    owner,
+  });
+  if (!tweet) {
+    throw new ApiError(500, "Something went wrong while creating your tweet");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet created Successfully"));
+});
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
-})
+  // TODO: get user tweets
+  const {userId} = req.params;
+  if (!userId) {
+    throw new ApiError(404, "Empty user id");
+  }
+  const tweetsList = await Tweet.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $project: {
+        __v: 0,
+      },
+    },
+  ]);
+  return res.json(new ApiResponse(200, tweetsList));
+});
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
-})
+  //TODO: update tweet
+  const {tweetId} = req.params;
+  const {content} = req.body;
+  if (!(tweetId && content)) {
+    throw new ApiError(404, "Empty Id or content");
+  }
+  const tweet = await Tweet.findByIdAndUpdate(tweetId, {
+    content,
+  });
+  if (!tweet) {
+    throw new ApiError(404, "Invalid tweet id check again");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "tweet updated Successfully"));
+});
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
-})
+  //TODO: delete tweet
+  const {tweetId} = req.params;
+  if (!tweetId) {
+    throw new ApiError(404, "Empty Id");
+  }
+  const tweet = await Tweet.findByIdAndDelete(tweetId);
+  if (!tweet) {
+    throw new ApiError(404, "Invalid tweet id check again");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "tweet deleted Successfully"));
+});
 
-export {
-    createTweet,
-    getUserTweets,
-    updateTweet,
-    deleteTweet
-}
+export {createTweet, getUserTweets, updateTweet, deleteTweet};
