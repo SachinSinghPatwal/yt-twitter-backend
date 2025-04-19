28b0521fff64e8d476af336fe85f7bb6af9a7ee1
sachinsinghpatwal <sachinsingh71004@gmail.com>
2025-04-19 12:45:27 +0530

    check

--- src/app.js
+++ src/app.js
@@ -14,9 +14,6 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use("*", (req, res) => {
-  res.status(404).json({message: "Route not found"});
-});
 
 //routes import
 import userRouter from "./routes/user.routes.js";
