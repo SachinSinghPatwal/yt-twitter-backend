ebe6ec85e15cdff2021c623ce5196089ddac8ee8
sachinsinghpatwal <sachinsingh71004@gmail.com>
2025-04-19 12:35:46 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use("*", (req, res) => {
 
 //routes import
 import userRouter from "./routes/user.routes.js";
-import healthCheckRouter from "./routes/healthCheck.routes.js";
+// import healthCheckRouter from "./routes/healthCheck.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import videoRouter from "./routes/video.routes.js";
@@ -30,7 +30,7 @@ import playlistRouter from "./routes/playlist.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
 
 //routes declaration
-app.use("/api/v1/healthCheck", healthCheckRouter);
+// app.use("/api/v1/healthCheck", healthCheckRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
